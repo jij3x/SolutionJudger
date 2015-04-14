bbf530249a7d1d0a4f7cc9dde7f583b638c9abcc
jianxioj <jijex@yahoo.com>
2015-04-13 18:28:26 -0700

    update

--- judges.py
+++ judges.py
@@ -15,6 +15,8 @@ def clonegraph(user_out, user_err, answer):
     sorted_out = []
     for line in user_out:
         arr = json.loads(line)
+        for node in arr:
+            node[1:] = sorted(node[1:])
         arr.sort()
         sorted_out.append(json.dumps(arr, separators=(",", ":")))
     result = general(sorted_out, user_err, answer)
@@ -23,6 +25,11 @@ def clonegraph(user_out, user_err, answer):
 
 
 def wordladders(user_out, user_err, answer):
-    result = general(user_out, user_err, answer)
+    sorted_out = []
+    for line in user_out:
+        arr = json.loads(line)
+        arr.sort()
+        sorted_out.append(json.dumps(arr, separators=(",", ":")))
+    result = general(sorted_out, user_err, answer)
     if result != "Accept":
         return result
