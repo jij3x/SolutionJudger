754cd61830895133f7165162a46d29126fd95996
jianxioj <jijex@yahoo.com>
2015-05-04 18:35:15 -0700

    update

---
+++ _problems_009/Reverse Linked List/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
