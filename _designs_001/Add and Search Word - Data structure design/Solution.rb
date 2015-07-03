0761996e8557895fbb164c40c78e17c3568f85dd
jianxioj <jijex@yahoo.com>
2015-07-02 21:56:20 -0700

    update

--- _designs_001/Add and Search Word - Data structure design/Solution.rb
+++ _designs_001/Add and Search Word - Data structure design/Solution.rb
@@ -2,12 +2,12 @@ class Solution
   def testWordDictionary(inp)
     wd, r = WordDictionary.new, []
     Serializer.deserializeInt(inp).times do
-        case Serializer.deserializeInt(inp)
-        when 0
-          wd.addWord(Serializer.deserializeString(inp))
-        when 1
-          r << wd.search(Serializer.deserializeString(inp))
-        end
+      case Serializer.deserializeInt(inp)
+      when 0
+        wd.addWord(Serializer.deserializeString(inp))
+      when 1
+        r << wd.search(Serializer.deserializeString(inp))
+      end
     end
     r
   end
