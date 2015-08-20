3c83f1465fb69cd1d6d6477729bb06138c2dc308
jianxioj <jijex@yahoo.com>
2015-08-19 19:40:17 -0700

    update

---
+++ _problems_011/Group Shifted Strings/convert_in.py
@@ -0,0 +1,11 @@
+import sys
+
+lines = sys.stdin.read().splitlines()
+print(len(lines))
+for line in lines:
+    line = line.replace(" ", "")
+    if line == "[]":
+        print(0)
+    else:
+        parts = line[1:-1].split(",")
+        print("{} {}".format(len(parts), " ".join(parts)))
