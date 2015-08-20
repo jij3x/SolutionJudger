0384f946102fbe08f5ea21aa1c28f69ed4a5155a
jijex <jijex@yahoo.com>
2015-08-20 07:46:03 -0700

    update

--- _problems_011/Meeting Rooms/Solution.java
+++ _problems_011/Meeting Rooms/Solution.java
@@ -1,19 +1,15 @@
 public class Solution {
     public boolean canAttendMeetings(List<Interval> intervals) {
-        if (intervals == null)
-            return false;
-
-        // Sort the intervals by start time
         Collections.sort(intervals, new Comparator<Interval>() {
-            public int compare(Interval a, Interval b) {
-                return a.start - b.start;
+            public int compare(Interval i1, Interval i2) {
+                return i1.start - i2.start;
             }
         });
 
-        for (int i = 1; i < intervals.size(); i++)
+        for (int i = 1; i < intervals.size(); i++) {
             if (intervals.get(i).start < intervals.get(i - 1).end)
                 return false;
-
+        }
         return true;
     }
 }
