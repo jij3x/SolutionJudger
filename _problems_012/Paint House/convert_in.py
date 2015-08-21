904adb96496a3609111e3aa0d4ba526ecc0f1c28
jianxioj <jijex@yahoo.com>
2015-08-21 16:17:44 -0700

    update

---
+++ _problems_012/Paint House/convert_in.py
@@ -0,0 +1,14 @@
+import sys
+import json
+
+lines = sys.stdin.read().splitlines()
+print(len(lines))
+for line in lines:
+    arr = json.loads(line)
+    print(len(arr), end="")
+    for row in arr:
+        if len(row) == 0:
+            print(" 0", end = "")
+        else:
+            print(" {} {}".format(str(len(row)), " ".join(str(x) for x in row)), end="")
+    print()
