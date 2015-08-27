c6ea82601a2c2f0f6ca2304b8a2ea29bce1ff1c6
jianxioj <jijex@yahoo.com>
2015-08-26 17:32:27 -0700

    update

---
+++ _problems_012/Closest Binary Search Tree Value/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int closestValue(TreeNode root, double target) {
+        int a = root.val;
+        TreeNode kid = target < a ? root.left : root.right;
+        if (kid == null) return a;
+        int b = closestValue(kid, target);
+        return Math.abs(a - target) < Math.abs(b - target) ? a : b;
+    }
+}
