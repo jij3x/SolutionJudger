a524c9c071dab0df04d20448143e77942bee3331
jianxioj <jijex@yahoo.com>
2015-09-03 18:28:36 -0700

    update

--- _problems_012/H-Index/Solution.java
+++ _problems_012/H-Index/Solution.java
@@ -1,10 +1,12 @@
 public class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int h = 0, i = citations.length;
-        for (int c : citations)
-            if (c > --i)
-                h += 1;
-        return h;
+        int[] memo1 = new int[citations.length + 1];
+        for (int num : citations)
+            memo1[Math.min(num, citations.length)]++;
+        for (int i = citations.length, t = 0; i > 0; i--) {
+            if ((t += memo1[i]) >= i)
+                return i;
+        }
+        return 0;
     }
 }
